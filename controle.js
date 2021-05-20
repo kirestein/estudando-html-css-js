d619424045866a3a83ad796fafe662859256e695
kirestein <erik.proenca2011@gmail.com>
2021-05-20 20:37:24 -0300

    Mexendo em estilos através do js

--- controle.js
+++ controle.js
@@ -1,4 +1,6 @@
 window.onload=function() {
     console.log("Hello Word");
     document.getElementsByTagName("h1")[0].innerHTML="Aula de JavaScript";
+    document.getElementById("titulo2").style.color="red";
+    document.querySelectorAll("p")[1].style.fontWeight="bold";
 };
